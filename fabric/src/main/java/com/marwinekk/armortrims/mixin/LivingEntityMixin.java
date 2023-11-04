75e94de83c303aebe9907beb6f27ed2420073617
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-11-04 01:13:22 -0400

    misc

---
+++ fabric/src/main/java/com/marwinekk/armortrims/mixin/LivingEntityMixin.java
@@ -0,0 +1,8 @@
+package com.marwinekk.armortrims.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+}
--- fabric/src/main/java/com/marwinekk/armortrims/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/com/marwinekk/armortrims/platform/FabricPlatformHelper.java
@@ -2,6 +2,8 @@ package com.marwinekk.armortrims.platform;
 
 import com.marwinekk.armortrims.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.EquipmentSlot;
 
 public class FabricPlatformHelper implements IPlatformHelper {
 
@@ -20,5 +22,15 @@ public class FabricPlatformHelper implements IPlatformHelper {
     public boolean isDevelopmentEnvironment() {
 
         return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+
+    @Override
+    public void sendAbilityPacket(EquipmentSlot slot) {
+
+    }
+
+    @Override
+    public void sendMobEffectPacket(MobEffect effect) {
+
     }
 }
